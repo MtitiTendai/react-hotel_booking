6b3fa8cf0634f554ee1fa66198383276d3e45e49
MtitiTendai <tendaimtiti10@gmail.com>
2025-08-19 08:47:18 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hotel-booking/', // 👈 IMPORTANT for GitHub Pages
   plugins: [
     react(),
     tailwindcss()
